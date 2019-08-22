a3ee4fb812e7e49363c47a4db995d11db1be13fd
Jhorman Orozco <jsojhor@gmail.com>
2019-08-21 22:04:11 -0500

    initial commit
    lol

---
+++ src/components/WeatherData.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+
+const WeatherData= () => (
+
+    <div>
+        <WeatherTemperature/>
+        <WeatherExtraInfo/>
+    </div>
+    
+);
+    
+
+
+export default WeatherData;
---
+++ src/components/WeatherLocation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+
+const WeatheLocation = () => (
+
+    <div>
+        <Location city ={"Medellin"}></Location>
+        <WeatherData></WeatherData>  
+    </div>
+    
+
+);
+
+export default WeatheLocation;
